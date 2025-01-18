2ff2b00e6e6e4691240509219a4b0c533f92107b
vidhya-balaji <vidhyabalaji.nina@gmail.com>
2025-01-18 20:56:11 +0530

    revert

--- src/components/ZoomView.js
+++ src/components/ZoomView.js
@@ -1,40 +1,16 @@
 import "./style.css"
+function ZoomView(imagesrc,event) {
 
-
-// function ZoomView(imagesrc, event) {
-
-//     console.log("ZoomView :"+{imagesrc}.imagesrc);
-//     console.log(event);
+    console.log("ZoomView :"+{imagesrc}.imagesrc);
+    console.log(event);
     
-//     // return (
 
-//     //     <div id="popOut">
-//     //         <p id="close">&#x2715</p>
-//     //         <img id="zoomimg" src={{imagesrc}.imagesrc} width="400px" alt="image1"></img>
-//     //     </div>
-//     // )
-    
-// }
+    return (
 
-function ZoomView(){
-    const container=document.getElementById('container')
-    const images=document.querySelectorAll('#gallery img')
-    const popout=document.getElementById("popOut")
-    const zoomimg=document.getElementById("zoomimg")
-
-    images.forEach(function(image){
-        image.addEventListener("click",function(){
-            container.style.display='none';
-            popout.style.display='block';
-            zoomimg.setAttribute('src',image.getAttribute('src'));
-        })
-    
-    });
-    
-    const closebutton=document.getElementById("close");
-    closebutton.addEventListener("click",function(){
-        container.style.display='block';
-            popout.style.display='none';
-    })
+        <div id="popOut">
+            <p id="close">&#x2715</p>
+            <img id="zoomimg" src={{imagesrc}.imagesrc} width="400px" alt="image1"></img>
+        </div>
+    )
 }
 export default ZoomView
